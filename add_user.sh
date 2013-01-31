c5d9f7567f4753c92b488d26bae2402144387fcf
Emerson Lopes Machado <emerson@fantasticatecnologia.com.br>
2013-01-31 19:57:12 -0200

    ...

--- add_user.sh
+++ add_user.sh
@@ -1,5 +1,17 @@
 source $PWD/config
 
 filename=$(basename "$1")
-scp -P $port $1 $user@$server:gitema/public_keys/$filename
-ssh -p $port $user@$server ./gitema/authorize_keys.sh
+username=${filename%.*}
+
+# scp -P $port $1 $user@$server:gitema/public_keys/$filename
+# ssh -p $port $user@$server ./gitema/authorize_keys.sh
+
+echo "
+host homologa
+    User git
+    Port $port
+    Hostname $server
+    PreferredAuthentications publickey
+    IdentityFile ~/.ssh/$username" >> ~/.ssh/config
+    
+    
--- authorize_keys.sh
+++ authorize_keys.sh
@@ -1,5 +1,7 @@
 rm -rf ~/.ssh/authorized_keys
-for i in $PWD/public_keys/* ; do
+touch ~/.ssh/authorized_keys
+chmod 0600 ~/.ssh/authorized_keys
+
+for i in $PWD/gitema/public_keys ; do
     cat $i >> ~/.ssh/authorized_keys
 done
-chmod 0600 ~/.ssh/authorized_keys
