d9751e6df0daedc23bc896f990fb5bf499720b6c
Emerson Lopes Machado <emerson@fantasticatecnologia.com.br>
2013-01-31 20:41:28 -0200

    new structure and new git-deploy command

---
+++ admin/add_user.sh
@@ -0,0 +1,17 @@
+source $PWD/config
+
+filename=$(basename "$1")
+username=${filename%.*}
+
+echo scp -P $port $1 $user@$server:$server_path/public_keys/$filename
+echo ssh -p $port $user@$server ./$server_path/authorize_keys.sh
+
+# echo "
+# host homologa
+#     User git
+#     Port $port
+#     Hostname $server
+#     PreferredAuthentications publickey
+#     IdentityFile ~/.ssh/$username" >> ~/.ssh/config
+    
+    
--- install.sh
+++
@@ -1,4 +0,0 @@
-chmod 0700 config
-chmod 0700 add_user.sh
-chmod 0700 authorize_keys.sh
-mkdir -p public_keys
